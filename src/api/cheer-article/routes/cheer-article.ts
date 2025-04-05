d152a6ecb52e1643b5cff38967d5b0739f4d5102
jamescurrandickinson <34398675+jamescurrandickinson@users.noreply.github.com>
2025-04-05 11:53:17 +0100

    Misc changes

---
+++ src/api/cheer-article/routes/cheer-article.ts
@@ -0,0 +1,7 @@
+/**
+ * cheer-article router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::cheer-article.cheer-article');
---
+++ src/api/cheer-article/services/cheer-article.ts
@@ -0,0 +1,7 @@
+/**
+ * cheer-article service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::cheer-article.cheer-article');
