bcfb5d1ea8509ac0ed65dad8c74f1d6c4f8e0c72
jamescurrandickinson <34398675+jamescurrandickinson@users.noreply.github.com>
2025-04-05 15:11:33 +0100

    Misc changes

--- config/database.ts
+++ config/database.ts
@@ -2,11 +2,11 @@ export default ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST", "127.0.0.1"),
+      host: env("DATABASE_HOST", "cheerfans-strapi-postgres-db-server.c3zqbhmlfgzz.eu-west-2.rds.amazonaws.com"),
       port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "strapi"),
-      user: env("DATABASE_USERNAME", ""),
-      password: env("DATABASE_PASSWORD", ""),
+      database: env("DATABASE_NAME", "cheerfans-strapi-postgres-db-server"),
+      user: env("DATABASE_USERNAME", "postgres"),
+      password: env("DATABASE_PASSWORD", "rqr<<x9*.)W>]lPi*:oeojvUfV?A"),
     },
     useNullAsDefault: true,
   },
