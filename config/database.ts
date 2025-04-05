60d9ae40af1161619f0180ddf23b7a2d64891f98
jamescurrandickinson <34398675+jamescurrandickinson@users.noreply.github.com>
2025-04-05 15:42:07 +0100

    Misc changes

--- config/database.ts
+++ config/database.ts
@@ -4,13 +4,13 @@ export default ({ env }) => ({
     connection: {
       host: env("DATABASE_HOST", "cheerfans-strapi-postgres-db-server.c3zqbhmlfgzz.eu-west-2.rds.amazonaws.com"),
       port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "cheerfans-strapi-postgres-db-server"),
+      database: env("DATABASE_NAME", "strapi"),
       user: env("DATABASE_USERNAME", "postgres"),
       password: env("DATABASE_PASSWORD", "rqr<<x9*.)W>]lPi*:oeojvUfV?A"),
       ssl: {
         rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
       },
     },
-    useNullAsDefault: false,
+    debug: false,
   },
 });
