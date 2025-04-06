5cd5c35567d1277b0c7a12d1cb87f738b83cb3c4
jamescurrandickinson <34398675+jamescurrandickinson@users.noreply.github.com>
2025-04-06 14:30:44 +0100

    Misc changes

--- config/server.ts
+++ config/server.ts
@@ -2,7 +2,7 @@ export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS').split(','),
+    keys: env.array('APP_KEYS'),
   },
 });
 
